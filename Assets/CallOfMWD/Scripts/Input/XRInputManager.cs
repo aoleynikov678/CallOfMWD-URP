e33f73100c8caa1e46141d89e9d7c44129a1eada
aoleynikov678 <a.o.oleynikov@gmail.com>
2020-04-28 13:13:53 +0300

    xr input dont destroy

---
+++ Assets/CallOfMWD/Scripts/Input/XRInputManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace lab.mwd
+{
+    public class XRInputManager : MonoBehaviour
+    {
+        private static XRInputManager instance;
+        
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+
+            instance = this;
+            
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
