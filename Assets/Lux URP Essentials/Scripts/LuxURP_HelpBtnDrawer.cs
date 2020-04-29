c52ceea5532d12cb91be4c671998a177fbc439ff
aoleynikov678 <a.o.oleynikov@gmail.com>
2020-04-29 14:22:24 +0300

    fixes of lux for build

--- Assets/Lux URP Essentials/Scripts/LuxURP_HelpBtnDrawer.cs
+++ Assets/Lux URP Essentials/Scripts/LuxURP_HelpBtnDrawer.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
+#if UNITY_EDITOR
 namespace LuxURPEssentials
 {
     [CustomPropertyDrawer(typeof(LuxURP_HelpBtn))]
@@ -34,3 +35,4 @@ namespace LuxURPEssentials
         }
     }
 }
+#endif
